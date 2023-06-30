b13cc7de5fd28f437d370eec8e996eacd34a1beb
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-06-30 11:10:04 +0430

    s03e12

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_DAMAGE = 10;
 const ENEMY_ATACK_DAMAGE = 15;
 const POWER_ATTACK_DAMAGE = 18;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 
@@ -9,6 +10,19 @@ let playerHealth = chosenMaxLife;
 
 initiatePlayerAndEnemyHealth(chosenMaxLife);
 
+
+function endTurn() {
+    const playerDamage = dealDamageToPlayer(ENEMY_ATACK_DAMAGE);
+    playerHealth -= playerDamage;
+
+    if (enemyHealth <= 0 && playerHealth > 0) {
+        alert("Hooora! You Won");
+    } else if (playerHealth <= 0 && enemyHealth > 0) {
+        alert(" you lost");
+    } else if (enemyHealth <= 0 && playerHealth <= 0) {
+        alert("draw!");
+    }
+}
 
 function attack(attackMode) {
     let baseDamage;
@@ -23,16 +37,7 @@ function attack(attackMode) {
 
     enemyHealth -= enemyDamage;
 
-    const playerDamage = dealDamageToPlayer(ENEMY_ATACK_DAMAGE);
-    playerHealth -= playerDamage;
-
-    if (enemyHealth <= 0 && playerHealth > 0) {
-        alert("Hooora! You Won");
-    } else if (playerHealth <= 0 && enemyHealth > 0) {
-        alert(" you lost");
-    } else if (enemyHealth <= 0 && playerHealth <= 0) {
-        alert("draw!");
-    }
+    endTurn();
 }
 
 
@@ -45,6 +50,24 @@ function powerAttackHandler() {
     attack('POWER')
 }
 
+function healPlayer() {
+    let healValue;
+
+    if (playerHealth >= chosenMaxLife - HEAL_VALUE) {
+        healValue = chosenMaxLife - playerHealth;
+    } else {
+        healValue = HEAL_VALUE;
+    }
+
+
+    increasePlayerHealth(healValue);
+
+    playerHealth += healValue;
+
+    endTurn();
+}
+
 
 attackButtonElm.addEventListener("click", attackHandler);
 strongAttackButtonElm.addEventListener("click", powerAttackHandler);
+healButtonElm.addEventListener("click", healPlayer);
